9f6d8e6c113424204bfd3e3903fdac3cc089b070
Carmen Nicolás Baixauli <carmennicolasbaixauli@MacBook-Pro-de-Carmen-Nicolas-Baixauli.local>
2016-02-08 11:34:18 +0100

    A Iyad le gusta mas que le llamen Idy

--- pos.py
+++ pos.py
@@ -1,5 +1,5 @@
 #Just add your name to the list to show that git pull/push worked for you
 
-gitRockStars = ["iyaaad,Begum"]
+gitRockStars = ["iyaaad,Begum, Carmen"]
 for i in gitRockStars:
     print ("Congrats " + i + " , you know how to use GIT\n");
