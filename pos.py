fc7dc225f297913d5d27a8dbee9e58bad9be2533
macbook <iyad.aqel1@gmail.com>
2016-02-08 23:03:03 +0100

    Very Initial Structure

--- pos.py
+++ pos.py
@@ -4,15 +4,15 @@
 
 
 #Show the user a menu to choose what he wants
-# print("Welcome to Pythunicorns POS. Dominating the european markets since 1913  ")
-userchoice = input("Please enter your option \n 1. for Sales \n 2. For reports \n 3. To close the day \n 4. To quit \n")
-while(int(userchoice) != 4):
-    #Do somestuff
-    print("The sales process goes here")
-    moreSales = input("More sales? Y/N")
-    if(moreSales=="Y"):
-        continue
-    elif(moreSales=="N"):
-        break
+print("Welcome to Pythunicorns POS. Dominating the european markets since 1913  ")
+moreOperations= True
+while(moreOperations==True ):
+    userChoice = input("Please enter your option \n 1. for Sales \n 2. For reports \n 3. To close the day \n")
+    if(int(userChoice)==1):
+        print("Sales")
+    elif(int(userChoice)==2):
+        print("Reports")
+    elif(int(userChoice)==3):
+        print("Close")
+        moreOperations = False
 print("Have a nice day")
-
