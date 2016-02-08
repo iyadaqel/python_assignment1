e34d87935b2ff79de9b110b06383fef6ef659e02
macbook <iyad.aqel1@gmail.com>
2016-02-08 22:33:58 +0100

    veeery initial structure

--- pos.py
+++ pos.py
@@ -1,5 +1,18 @@
-#Just add your name to the list to show that git pull/push worked for you
+#Import all the modules
 
-gitRockStars = ["Idy, Begum, Carmen"]
-for i in gitRockStars:
-    print ("Congrats " + i + " , you know how to use GIT\n");
+
+
+
+#Show the user a menu to choose what he wants
+# print("Welcome to Pythunicorns POS. Dominating the european markets since 1913  ")
+userchoice = input("Please enter your option \n 1. for Sales \n 2. For reports \n 3. To close the day \n 4. To quit \n")
+while(int(userchoice) != 4):
+    #Do somestuff
+    print("The sales process goes here")
+    moreSales = input("More sales? Y/N")
+    if(moreSales=="Y"):
+        continue
+    elif(moreSales=="N"):
+        break
+print("Have a nice day")
+
