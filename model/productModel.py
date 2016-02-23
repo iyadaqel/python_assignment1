5d41872a11c97700d0e07ef7a37b03020cd05367
CarmenNB <carmen.nibai@gmail.com>
2016-02-23 14:50:25 +0100

    Bomboncitooooos
    os miro y me derrito

--- model/productModel.py
+++ model/productModel.py
@@ -1,19 +1,28 @@
 class Product (object):
                total=0
+               productListita=[]
 
                def __init__ (self, SKU, price):
                               Product.total+=1
                               self.SKU=SKU
                               self.price=price
+                              Product.productListita.append(self)
 
 
                @staticmethod
                def totals():
                               print("The total number of products is", Product.total)
 
+               @staticmethod
+               def productList():
+                   for i in range (Product.total):
+                       print(Product.productListita[i])
+
                def __str__(self):
-                              rep ="#SKU: " + self.SKU + "#Price: "+ self.price
+                              rep ="#SKU:" + self.SKU + " #Price:"+ self.price
                               return rep
 
-               def getPrice (self):
+               def getPrice (self, ):
                               return self.price
+
+
