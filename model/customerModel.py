234a24b6028acae49629d29a9bf62d9229b908ce
begumirkdas <birkdas@gmail.com>
2016-02-23 15:20:10 +0100

    customerModel_3

--- model/customerModel.py
+++ model/customerModel.py
@@ -13,7 +13,8 @@ self.__customers=[]
         self.__customerID=new_id
         self.__customers.append(customerName)
     def searchCustomer(self,customerID):
-        if isinstance(self.__customerID,str)==True:
+        for self.__customers in self.__customers(self.__customers[1:]):
+        if self.__customerID is str:
             searchval=1
         else:
             searchval=0
