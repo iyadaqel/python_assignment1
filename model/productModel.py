0e25bab93b246730611be9c4b2cfb05efec16129
CarmenNB <carmen.nibai@gmail.com>
2016-02-23 17:46:06 +0100

    Bomboncito
    te miro y me derrito

--- model/productModel.py
+++ model/productModel.py
@@ -1,12 +1,12 @@
 class Product (object):
                total=0
-               productListita=[]
+               __productListita=[]
 
                def __init__ (self, SKU, price):
                               Product.total+=1
-                              self.SKU=SKU
-                              self.price=price
-                              Product.productListita.append(self)
+                              self.__SKU=SKU
+                              self.__price=price
+                              Product.__productListita.append(self)
 
 
                @staticmethod
@@ -16,21 +16,21 @@ class Product (object):
                @staticmethod
                def productList():
                    for i in range (Product.total):
-                       print(Product.productListita[i])
+                       print(Product.__productListita[i])
 
                @staticmethod
                def searchSKU(sSKU):
                    a=0
                    for i in range (Product.total):
-                       comparar=str(Product.productListita[i])
+                       comparar=str(Product.__productListita[i])
                        if ((comparar.find(sSKU))!= (-1)):
-                           print(Product.productListita[i])
+                           print(Product.__productListita[i])
                            a=1
                    if a==0:
                        print("The SKU number "+ sSKU + " does not exist")
 
 
                def __str__(self):
-                              rep ="#SKU:" + self.SKU + " #Price:"+ self.price
+                              rep ="#SKU:" + self.__SKU + " #Price:"+ self.__price
                               return rep
 
