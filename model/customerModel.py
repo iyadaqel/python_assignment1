0a2d49b883570e33e4c8409e47c18a3fad20c9fc
begumirkdas <birkdas@gmail.com>
2016-02-23 14:46:58 +0100

    customerModel_attempt2

--- model/customerModel.py
+++ model/customerModel.py
@@ -4,12 +4,20 @@ class Customer(object):
         self.__customerID=customerID
         self.__customerName=customerName
         Customer.total+=1
-        self.__customers=[]
+self.__customers=[]
     def totalCustomers(self):
         return Customer.total
     def getId(self):
         return self.__customerID
     def setId(self,new_id):
         self.__customerID=new_id
-    def createCustomer(self, customerName):
         self.__customers.append(customerName)
+    def searchCustomer(self,customerID):
+        if isinstance(self.__customerID,str)==True:
+            searchval=1
+        else:
+            searchval=0
+            print("Not valid user ID")
+        return(searchval)
+            
+        
