f7d987e6a00933c3d58c3ec393fff63c1e900f08
moniram <mon_4isa@hotmail.com>
2016-03-02 16:14:45 +0100

    Sale Model
    Sale model is working now. I put an example in the end for testing

--- model/saleModel.py
+++ model/saleModel.py
@@ -2,11 +2,11 @@ class Sale(object):
     totalDailySales = 0
     saleList=[]
 
-    def __init__(self,product,PayMethod,Customer):
-        self.product = product
+    def __init__(self,Product,PayMethod,Customer):
+        self.product = Product
         self.PayMethod = PayMethod
         self.Customer = Customer
-        Sale.totalDailySales += 1
+        Sale.totalDailySales+=1
         Sale.saleList.append(self)
 
 
@@ -14,16 +14,11 @@ class Sale(object):
         print("The total daily sales are", Sale.totalDailySales)
 
     def getSaleList():
-        for i in range (len(Sale.saleList)):
-            print(Sale.saleList[i])
+        for i in range (Sale.totalDailySales):
+                       print(Sale.saleList[i].product, Sale.saleList[i].PayMethod, Sale.saleList[i].Customer)
 
-
-
+    
         
-sale1=Sale("kk","cc","99")
-print
-print(sale1.product, sale1.PayMethod, sale1.Customer)
-Sale.getTotalDailySales()
-print(len(Sale.saleList))
-print(Sale.saleList[0])
-Sale.getSaleList()
+#sale1=Sale("kk","cc","99")
+#Sale.getTotalDailySales()
+#Sale.getSaleList()
